509ea5cb01065a332bbe855f50534468c37f9f97
DESKTOP-NPMCN1O\yongyi <qinyongyi@nst168.com>
2018-04-17 14:45:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yongyi/myhotuptate/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.yongyi.myhotuptate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "这是一个fix bug", Toast.LENGTH_LONG).show();
+    }
+}
